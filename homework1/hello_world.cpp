de0b1ec5a8742dfb986164e3dc856f9392d1cb66
Dmitry740 <dimon-_mail.ru>
2022-07-09 18:56:57 +0300

    homework

--- homework1/hello_world.cpp
+++ homework1/hello_world.cpp
@@ -2,6 +2,17 @@
 
 #include <iostream>
 
-void example_function() {
-	std::cout << "Hello from example function!" << std::endl;
+void todo_move_me_out()
+{
+	std::cout << "This function should be located in the separated source file" << std::endl;
+}
+
+int main()
+{
+	std::cout << "Hello, World!" << std::endl;
+
+	example_function();
+	todo_move_me_out();
+
+	return 0;
 }
