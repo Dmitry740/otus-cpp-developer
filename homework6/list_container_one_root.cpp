9dd4e7000a7735b5cf18aab1f606f18bc862d4ae
Dmitry740 <dimon-_mail.ru>
2022-09-13 23:51:47 +0300

    homework 6

---
+++ homework6/list_container_one_root.cpp
@@ -0,0 +1,229 @@
+#include <iostream>
+
+template <typename T>
+class ListContainer {
+  struct Node {
+    Node* next;
+    T data;
+  };
+
+ public:
+  ListContainer() : m_size{0}, m_first{nullptr}, m_last{nullptr} {}
+
+  ListContainer(const ListContainer& oter)
+      : ListContainer{other.m_first(), other.m_last()} {
+    std::cout << "ListContainer(const ListContainer&)" << std::endl;
+  }
+
+  ListContainer(ListContainer&& other) {
+    m_first = other.m_first;
+    other.m_first = nullptr;
+    m_last = other.m_last;
+    other.m_last = nullptr;
+    m_size = other.m_size;
+    m_size = 0;
+  }
+
+  ~ListContainer() {
+    delete[] m_first;
+    delete[] m_last;
+    std::cout << "~ListContainer" << std::endl;
+  }
+
+  ListContainer& operator=(const ListContainer& rhs) {
+    std::cout << "ListContainer::operator=" << std::endl;
+    ListContainer temp{rhs};
+    Node* last = m_last;
+    m_last = temp.m_last;
+    temp.m_last = last;
+
+    Node* first = m_first;
+    m_first = temp.m_first;
+    temp.m_first = first;
+
+    size_t size = m_size;
+    m_size = temp.m_size;
+    temp.m_size = size;
+
+    return *this;
+  }
+
+  ListContainer& operator=(ListContainer&& other) {
+    std::cout << "&&ListContainer::operator=" << std::endl;
+    ListContainer temp{std::move(rhs)};
+    return *this = temp;
+  }
+
+  bool push_back(const T& value) {
+    Node* new_node = new Node{};
+    new_node->next = nullptr;
+    new_node->data = value;
+
+    if (m_size == 0) {
+      m_first = std::move(new_node);
+    }
+
+    if (m_size >= 1) {
+      m_last->next = std::move(new_node);
+    }
+    m_last = std::move(new_node);
+
+    ++m_size;
+
+    return true;
+  }
+
+  bool insert(size_t pos, const T& value) {
+    Node* first = std::move(m_first);
+    if (pos == 0) {
+      Node* new_node = new Node{};
+      new_node->next = first;
+      new_node->data = std::move(value);
+      m_first = std::move(new_node);
+    } else {
+      size_t i = 0;
+      while (i != (pos - 1)) {
+        first = first->next;
+        ++i;
+      }
+      Node* prev = std::move(first);
+      first = first->next;
+
+      Node* new_node = new Node{};
+      new_node->next = first;
+      new_node->data = std::move(value);
+      prev->next = std::move(new_node);
+    }
+
+    ++m_size;
+
+    return true;
+  }
+
+  bool erase(const size_t pos) {
+    Node* first = m_first;
+    if (pos >= m_size) {
+      return false;
+    } else {
+      size_t i = 1;
+      while (i != (pos - 1)) {
+        first = first->next;
+        ++i;
+      }
+      Node* prev = std::move(first);
+      Node* del = first->next;
+      first = del->next;
+      prev->next = std::move(first);
+
+      delete[] del;
+
+      --m_size;
+    }
+    return true;
+  }
+
+  bool getelements() const {
+    for (Node* elem = m_first; elem != m_last->next; elem = elem->next) {
+      std::cout << elem->data << ' ';
+    }
+    return true;
+  }
+
+  // struct Iterator {
+  //   T& operator*() const {
+  //     for (Node* elem = m_first; elem != m_last->next; elem = elem->next) {
+  //       return elem->data;
+  //     }
+  //   }
+
+  //   Iterator& operator++() { return *this; }
+  //   Iterator& operator++(int) {
+  //     Iterator it(*this);
+  //     ++*this;
+
+  //     return it;
+  //   }
+
+  //   bool operator==(const Iterator& it) const {
+  //     if (m_last == it.m_last) return true;
+  //   }
+
+  //   bool operator!=(const Iterator& it) const { return !(it == *this); }
+  // };
+
+  // Iterator begin() { return m_first->data; }
+
+  // Iterator end() { return m_last->data; }
+
+  const T operator[](size_t index) const {
+    size_t i = 0;
+    Node* first = m_first;
+    while (i != index) {
+      first = first->next;
+      ++i;
+    }
+    return first->data;
+  }
+
+  size_t size() const { return m_size; }
+
+ private:
+  size_t m_size;
+  Node* m_first;
+  Node* m_last;
+};
+
+template <typename T>
+void print_container(ListContainer<T>& container) {
+  std::cout << "Elements: ";
+  container.getelements();
+  std::cout << std::endl;
+
+  std::cout << "Get 6th elenent: " << container[5];
+  std::cout << std::endl;
+
+  std::cout << "Size: " << container.size() << std::endl;
+  std::cout << std::endl << std::endl;
+}
+
+void test_container() {
+  ListContainer<int> int_container;
+
+  int_container.push_back(0);
+  int_container.push_back(1);
+  int_container.push_back(2);
+  int_container.push_back(3);
+  int_container.push_back(4);
+  int_container.push_back(5);
+  int_container.push_back(6);
+  int_container.push_back(7);
+  int_container.push_back(8);
+  int_container.push_back(9);
+
+  // for (auto iter = int_container.begin(); iter != int_container.end();
+  // ++iter) {
+  //   std::cout << *iter << std::endl;
+  // }
+
+  print_container(int_container);
+
+  int_container.erase(7);
+  int_container.erase(5);
+  int_container.erase(3);
+
+  print_container(int_container);
+
+  int_container.insert(0, 10);
+
+  print_container(int_container);
+
+  int_container.insert(4, 20);
+
+  print_container(int_container);
+
+  int_container.push_back(30);
+
+  print_container(int_container);
+}
+
+int main() { test_container(); }
---
+++ homework6/successive_container.cpp
@@ -0,0 +1,216 @@
+#include <iostream>
+
+template <typename T>
+class SuccessiveContainer {
+ public:
+  SuccessiveContainer() : m_size{0}, m_cap{0}, m_region{nullptr} {}
+
+  SuccessiveContainer(const SuccessiveContainer& other)
+      : SuccessiveContainer{other.m_region()} {
+    std::cout << "SuccessiveContainer(const SuccessiveContainer&" << std::endl;
+  }
+
+  SuccessiveContainer(SuccessiveContainer&& other) {
+    m_cap = other.m_cap;
+    m_size = other.m_size;
+    m_region = other.m_region;
+
+    other.m_cap = 0;
+    other.m_size = 0;
+    other.m_region = nullptr;
+  }
+
+  ~SuccessiveContainer() {
+    delete[] m_region;
+    std::cout << "~SuccessiveContainer" << std::endl;
+  }
+
+  SuccessiveContainer& operator=(const SuccessiveContainer& rhs) {
+    std::cout << "SuccessiveContainer::operator=" << std::endl;
+    SuccessiveContainer temp{rhs};
+    T* data = m_region;
+    m_region = temp.m_region;
+    temp.m_region = data;
+
+    size_t size = m_size;
+    m_size = temp.m_size;
+    temp.m_size = size;
+
+    size_t cap = m_cap;
+    m_cap = temp.m_cap;
+    temp.m_cap = cap;
+
+    return *this;
+  }
+
+  SuccessiveContainer& operator=(SuccessiveContainer&& rhs) {
+    std::cout << "&&SuccessiveContainer::operator=" << std::endl;
+    SuccessiveContainer temp{std::move(rhs)};
+    return *this = temp;
+  }
+
+  bool push_back(const T& val) {
+    if (m_size < m_cap) {
+      m_region[m_size] = std::move(val);
+      ++m_size;
+    } else {
+      T* new_region = new T[((m_size + 1) * 2)];
+      m_cap = ((m_size + 1) * 2);
+
+      for (size_t i = 0; i < m_size; ++i) {
+        new_region[i] = m_region[i];
+      }
+
+      new_region[m_size] = std::move(val);
+      delete[] m_region;
+      m_region = std::move(new_region);
+
+      ++m_size;
+    }
+    return true;
+  }
+
+  bool insert(const size_t& pos, const T& val) {
+    if (m_size < m_cap) {
+      for (size_t i = m_size; i != pos; --i) {
+        m_region[i] = m_region[i - 1];
+      }
+      m_region[pos] = std::move(val);
+      ++m_size;
+    } else {
+      T* new_region = new T[((m_size + 1) * 2)];
+      m_cap = ((m_size + 1) * 2);
+
+      for (size_t i = 0; i < pos; ++i) {
+        new_region[i] = m_region[i];
+      }
+
+      for (size_t i = m_size; i != pos; --i) {
+        new_region[i] = m_region[i - 1];
+      }
+
+      new_region[pos] = std::move(val);
+
+      delete[] m_region;
+      m_region = std::move(new_region);
+      ++m_size;
+    }
+    return true;
+  }
+
+  bool erase(const size_t pos) {
+    if (pos >= m_size) {
+      return false;
+    }
+
+    for (size_t i = pos - 1; i < m_size + 1; ++i) {
+      m_region[i] = m_region[i + 1];
+    }
+    --m_size;
+
+    return true;
+  }
+
+  bool empty() {
+    if (m_size == 0) {
+      std::cout << "Error: no elements in container" << std::endl << std::endl;
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  // struct Iterator {
+  //   int index;
+  //   T& get() { return m_region[index]; }
+  //   T& operator*() const { return m_region[index]; }
+  //   Iterator& operator++() { return *this; }
+  //   Iterator& operator++(int) {
+  //     Iterator it(*this);
+  //     ++*this;
+
+  //     return it;
+  //   }
+
+  //   bool operator==(const Iterator& it) const {
+  //     if (this == it) {
+  //       return true;
+  //     }
+  //   }
+
+  //   bool operator!=(const Iterator& it) const { return !(this == it); }
+  // };
+
+  // Iterator begin() { return Iterator(index = 0); }
+
+  // Iterator end() { return Iterator(m_size + 1); }
+
+  T& operator[](int i) { return m_region[i]; }
+
+  size_t size() const { return m_size; }
+
+  size_t capacity() const { return m_cap; }
+
+ private:
+  size_t m_size;
+  size_t m_cap;
+  T* m_region{};
+};
+
+template <typename T>
+void print_container(SuccessiveContainer<T>& container) {
+  std::cout << "Elements: ";
+  for (auto i = 0; i < container.size(); ++i) {
+    std::cout << container[i] << ' ';
+  }
+  std::cout << std::endl;
+
+  std::cout << "Size: " << container.size() << std::endl;
+  std::cout << std::endl << std::endl;
+}
+
+void test_container() {
+  SuccessiveContainer<int> int_container;
+
+  int_container.push_back(0);
+  int_container.push_back(1);
+  int_container.push_back(2);
+  int_container.push_back(3);
+  int_container.push_back(4);
+  int_container.push_back(5);
+  int_container.push_back(6);
+  int_container.push_back(7);
+  int_container.push_back(8);
+  int_container.push_back(9);
+
+  // for (auto iter = int_container.begin(); iter != int_container.end();
+  // ++iter) {
+  //   std::cout << *iter << std::endl;
+  // }
+
+  print_container(int_container);
+
+  int_container.erase(7);
+  int_container.erase(5);
+  int_container.erase(3);
+
+  print_container(int_container);
+
+  int_container.insert(0, 10);
+
+  print_container(int_container);
+
+  int_container.insert(4, 20);
+
+  print_container(int_container);
+
+  int_container.push_back(30);
+
+  print_container(int_container);
+}
+
+int main() {
+  test_container();
+
+  return 0;
+}
